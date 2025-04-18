3c3d1e7e3a310b0f62922054034447ec6b1b373c
jyc <jiangyachun811@gmail.com>
2025-04-18 18:06:31 +0800

    feat: init

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,9 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        "type-enum": [2, "always", ["foo"]],
-        "header-max-length": [1, "always", 10]
+        "type-enum": [2, "always", ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
+        'scope-empty': [2, 'always'], // scope 不可为空
+        "header-max-length": [2, "always", 30]
     },
+    helpUrl: "https://commitlint.js.org/reference/rules-configuration.html#rules-configuration"
 };
